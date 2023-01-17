338c557e5625183a13ed8cf2636ea6d1151d0b6f
nico0704 <nico.schroeder@informatik.hs-fulda.de>
2023-01-17 15:11:17 +0100

    third commit

--- lib/controller/kurs_controller.dart
+++ lib/controller/kurs_controller.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import '../model/model.dart';
 
 class KursController extends GetxController {
+  // var kurse = List<Kurs>.empty(growable: true).obs; -> geht auch, da sich dann ja die Liste ändert
   var model = Model().obs;
   var changed = 0.obs;
 
